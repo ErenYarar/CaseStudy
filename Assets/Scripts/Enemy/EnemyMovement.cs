19657c9acfbfce2f640d0f36ff12125b5192d5f9
Eren Yarar <eren2yarar1@gmail.com>
2023-07-07 22:48:28 +0300

    Enemy attack each other added

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    NavMeshAgent nva;
-    Rigidbody enemyRb;
-    Transform player;
+    private NavMeshAgent nva;
+    private Rigidbody enemyRb;
+    private GameObject[] targets;
     private Animator animator;
     private Vector3 initialPosition;
 
@@ -16,18 +17,48 @@ public class EnemyMovement : MonoBehaviour
         nva = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         enemyRb = GetComponent<Rigidbody>();
-        player = GameObject.FindWithTag("Player").transform;
         initialPosition = transform.position;
+
+        // Hedefleri ayarla: Oyuncu ve diğer düşmanlar
+        targets = GameObject.FindGameObjectsWithTag("Enemy");
+        targets = targets.Concat(new[] { GameObject.FindGameObjectWithTag("Player") }).ToArray();
     }
 
     private void FixedUpdate()
     {
-        nva.destination = player.position;
+        GameObject closestTarget = FindClosestTarget();
+        if (closestTarget != null)
+        {
+            // En yakın hedefe doğru hareket et
+            nva.destination = closestTarget.transform.position;
+        }
+    }
+
+    private GameObject FindClosestTarget()
+    {
+        GameObject closestTarget = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject target in targets)
+        {
+            if (target != gameObject) // Kendi kendini hedef olarak alma
+            {
+                float distance = Vector3.Distance(target.transform.position, currentPosition);
+                if (distance < closestDistance)
+                {
+                    closestTarget = target;
+                    closestDistance = distance;
+                }
+            }
+        }
+
+        return closestTarget;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
         {
             animator.SetBool("isFloating", true);
             Vector3 moveDirection = enemyRb.transform.position - collision.transform.position;
